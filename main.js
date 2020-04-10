b2788dd80175a75c003032b4c092b2ecaaa96cf7
Bogdan <bogdan@En760.kowaltchook.com>
2020-04-10 14:49:30 -0400

    22 passed, 22 total

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
 class Vehicle {
+  constructor(name, wheelCount, maxSpeed, speed = 0) {
+    this.name = name;
+    this.wheelCount = wheelCount;
+    this.maxSpeed = maxSpeed;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    let incrementedSpeed = this.maxSpeed / 3
+    this.speed += incrementedSpeed;
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    
+  }
 }
 
-class Car {
+class Car extends Vehicle{
+  constructor(name, maxSpeed, speed) {
+    super(name, 4, maxSpeed, speed);
+  }
+
+  drive() {
+    return `Vroom. `;
+  }
 }
 
-class Ferrari {
+class Ferrari extends Car {
+  constructor() {
+    super("Ferrari", 180);
+  }
+  drive() {
+    return super.drive() + "I'm a Ferrari!";
+  }
+
+  accelerate() {
+    let incrementedSpeed = this.maxSpeed / 2;
+    this.speed += incrementedSpeed;
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    
+  }
 }
 
 
